c4b916af9e8cd2cf09ac98cfbbac6ce6498875d9
bbanaszk <bbanaszk@asu.edu>
2024-10-19 18:38:41 +0300

    updating NEXT image

--- src/app/LearnReactPart3/MemeGenerator/components/Form.tsx
+++ src/app/LearnReactPart3/MemeGenerator/components/Form.tsx
@@ -1,28 +1,71 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import takeMyMoney from '/public/images/takemymoney.png';
+import memesData from "./memesData";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import {Typography} from "@mui/material";
+
 export default function Form() {
+    let [currentImage, setNewImage] = useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomIndex = Math.floor(Math.random() * memesArray.length)
+        setNewImage(memesArray[randomIndex].url)
+    }
+    
     return(
         <div>
             <div>
                 <div className='input-group'>
                     <div className='top-part'>
                         <Typography>Top text</Typography>
-                        <TextField variant='outlined' size='small' style={{width:222}}/>
+                        <TextField variant='outlined' size='small' style={{width: 222}}/>
                     </div>
                     <div className='bottom-part'>
                         <Typography>Bottom text</Typography>
-                        <TextField variant='outlined' size='small' style={{width:'222px'}}/>
+                        <TextField variant='outlined' size='small' style={{width: 222}}/>
                     </div>
                 </div>
-                <Button className='meme-button' style={{textTransform:'none'}}>
+                <Button className='meme-button' onClick={getMemeImage} style={{textTransform: 'none'}}>
                     Get a new meme image 🖼
                 </Button>
             </div>
-            <Image className="meme-image" src={takeMyMoney} alt="bart from futureama"/>
+            {/*<div style={{*/}
+            {/*    display: 'flex',*/}
+            {/*    justifyContent: 'center',*/}
+            {/*    alignItems: 'center',*/}
+            {/*    width: '100%',*/}
+            {/*    height: 'auto',*/}
+            {/*    marginTop: '20px'*/}
+            {/*}}>*/}
+            {/*    {currentImage && (*/}
+            {/*        <img*/}
+            {/*            className="meme-image"*/}
+            {/*            src={currentImage}*/}
+            {/*            alt="Meme Image"*/}
+            {/*            style={{maxWidth: '100%', height: 'auto', display: 'block'}}*/}
+            {/*        />*/}
+            {/*    )}*/}
+            {/*</div>*/}
+            {/*<Image className="meme-image"*/}
+            {/*       src={currentImage}*/}
+            {/*       alt=""*/}
+            {/*       layout="fill"*/}
+            {/*       objectFit={'contain'}/>*/}
+            {currentImage && (
+                <div style={{ position: 'relative', width: '100%', height: '300px', marginTop: '20px' }}>
+                    <Image
+                        className="meme-image"
+                        src={currentImage}
+                        alt="Meme Image"
+                        fill
+                        objectFit="contain"
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    />
+                </div>
+            )}
         </div>
     )
 }
